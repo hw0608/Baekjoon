bd0227b889d3c6f881383d1fc35487fad211d50f
민희원 <hwon.minn@gmail.com>
2025-05-23 16:05:45 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 10980 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,65 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char board[1000][1000];
-int jDist[1000][1000];
-int fDist[1000][1000];
-int dx[4] = { 0,0,-1,1 };
-int dy[4] = { 1,-1,0,0 };
+int R, C;
+char board[1002][1002];
+int dist_jihoon[1002][1002];
+int dist_fire[1002][1002];
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
 
-	int R, C;
+bool OOB(int  a, int b) {
+	return a < 0 || a >= R || b < 0 || b >= C;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	queue<pair<int, int>> q_jihoon;
+	queue<pair<int, int>> q_fire;
+
+	fill(&dist_jihoon[0][0], &dist_jihoon[1001][1002], -1);
+	fill(&dist_fire[0][0], &dist_fire[1001][1002], -1);
+
 	cin >> R >> C;
-
-	queue<pair<int, int>> jQ;
-	queue<pair<int, int>> fQ;
-	memset(jDist, -1, sizeof(jDist));
-	memset(fDist, -1, sizeof(fDist));
-	bool flag = false;
 
 	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
 			cin >> board[i][j];
-			if (board[i][j] == 'J') { jQ.push({ i,j }); jDist[i][j] = 0; }
-			else if (board[i][j] == 'F') {fQ.push({ i,j }); fDist[i][j] = 0;}
-		}
-	}
 
-	while (!fQ.empty()) {
-		auto cur = fQ.front(); fQ.pop();
-
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
-
-			if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
-			if (board[nx][ny] == '#' || fDist[nx][ny] >= 0) continue;
-			fDist[nx][ny] = fDist[cur.first][cur.second] + 1;
-			fQ.push({ nx,ny });
-		}
-	}
-
-	while (!jQ.empty()) {
-		auto cur = jQ.front(); jQ.pop();
-
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
-
-			if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
-				cout << jDist[cur.first][cur.second] + 1;
-				flag = true;
-				break;
+			if (board[i][j] == 'J')
+			{
+				dist_jihoon[i][j] = 0;
+				q_jihoon.push({ i,j });
 			}
-			if (board[nx][ny] == '#' || jDist[nx][ny] >= 0) continue;
-			if (fDist[nx][ny]!=-1 && fDist[nx][ny] <= jDist[cur.first][cur.second] + 1) continue;
-			jDist[nx][ny] = jDist[cur.first][cur.second] + 1;
-			jQ.push({ nx, ny });
+			else if (board[i][j] == 'F')
+			{
+				dist_fire[i][j] = 0;
+				q_fire.push({ i,j });
+			}
 		}
-		if (flag) break;
 	}
-	if (!flag) cout << "IMPOSSIBLE\n";
+
+	while (!q_fire.empty()) {
+		auto cur = q_fire.front(); q_fire.pop();
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = cur.first + dx[dir];
+			int ny = cur.second + dy[dir];
+
+			if (OOB(nx, ny)) continue;
+			if (board[nx][ny] == '#' || dist_fire[nx][ny] != -1) continue;
+			dist_fire[nx][ny] = dist_fire[cur.first][cur.second] + 1;
+			q_fire.push({ nx,ny });
+		}
+	}
+
+	while (!q_jihoon.empty()) {
+		auto cur = q_jihoon.front(); q_jihoon.pop();
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = cur.first + dx[dir];
+			int ny = cur.second + dy[dir];
+
+			if (OOB(nx, ny)) {
+				if (dist_fire[cur.first][cur.second] > dist_jihoon[cur.first][cur.second] || dist_fire[cur.first][cur.second] == -1) {
+					cout << dist_jihoon[cur.first][cur.second] + 1;
+					return 0;
+				}
+			}
+
+			if (board[nx][ny] == '#' || dist_jihoon[nx][ny] != -1) continue;
+			if (dist_fire[nx][ny] != -1 && dist_fire[nx][ny] <= dist_jihoon[cur.first][cur.second] + 1) continue;
+			dist_jihoon[nx][ny] = dist_jihoon[cur.first][cur.second] + 1;
+			q_jihoon.push({ nx,ny });
+		}
+	}
+
+	cout << "IMPOSSIBLE";
 }
